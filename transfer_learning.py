907f5c3706120205de5d18c745e41b4b6bb05800
Wabinab <chowjunwei37@gmail.com>
2021-06-26 09:57:14 +0000

    commit

--- transfer_learning.py
+++ transfer_learning.py
@@ -4,9 +4,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+
 from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
 from torchvision import datasets, models, transforms
+
+from utils import ArielMLDataset, BaselineLSTM, ChallengeMetric, Baseline, simple_transform
 
 import copy
 import matplotlib.pyplot as plt
@@ -16,28 +19,87 @@ import pathlib
 import time
 from tqdm import tqdm
 
-# plt.ion()  # Interactive mode. 
+# plt.ion()  # Interactive mode.
 
 project_dir = pathlib.Path(__file__).parent.absolute()
 
 train_size = 110000
 val_size = 15600
+batch_size = 25
+save_from = 1
 
-dataset_train = ArielMLDataset(lc_train_path, params_train_path, shuffle=True, start_ind=0,
-                                max_size=train_size, transform=simple_transform, device=device,
+
+def train_model(model, criterion, opt, scheduler, loader_train, loader_val, 
+                device="cpu", num_epochs=25):
+    """
+    :var model: (Pytorch savedmodel format, any .pt, .pth, etc) The model to use for transfer learning. 
+    """
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_val_score = 0.0
+    challenge_metric = ChallengeMetric()
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch}/{num_epochs - 1}")
+        val_score = 0
+        model.train()
+
+        for k, item in enumerate(tqdm(loader_train)):
+            pred = model(item['lc'])
+            loss = criterion(item['target'], pred)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            scheduler.step()
+            # train_loss += loss.detach().item()
+
+        model.eval()
+
+        for k, item in tqdm(enumerate(loader_val)):
+            pred, _ = model(item['lc'])
+            loss = criterion(item['target'], pred)
+            score = challenge_metric.score(item['target'], pred)
+            # val_loss += loss.detach().item()
+            val_score += score.detach().item()
+
+        val_score /= len(loader_val)
+
+        print('Val score', round(val_score, 2))
+
+        val_scores += [val_score]
+
+        if epoch >= save_from and val_score > best_val_score:
+            best_val_score = val_score
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+            torch.save({
+                "epoch": epoch,
+                "opt": opt.state_dict(),
+                "scheduler": scheduler.state_dict(),
+                "model": model.state_dict()
+            }, "Best_trained_model_resnet.pt")
+
+    model.load_state_dict(best_model_wts)
+    
+    return val_scores, model
+
+
+def main():
+    dataset_train = ArielMLDataset(lc_train_path, params_train_path, shuffle=True, start_ind=0,
+                                    max_size=train_size, transform=simple_transform, device=device,
+                                    transpose=False)
+        # Validation
+    dataset_val = ArielMLDataset(lc_train_path, params_train_path, shuffle=True, start_ind=train_size,
+                                max_size=val_size, transform=simple_transform, device=device,
                                 transpose=False)
-    # Validation
-dataset_val = ArielMLDataset(lc_train_path, params_train_path, shuffle=True, start_ind=train_size,
-                            max_size=val_size, transform=simple_transform, device=device,
-                            transpose=False)
 
-# paths to data dirs
-lc_train_path = project_dir / \
-    "/home/chowjunwei37/Documents/data/training_set/noisy_train"
-params_train_path = project_dir / \
-    "/home/chowjunwei37/Documents/data/training_set/params_train"
+    # paths to data dirs
+    lc_train_path = project_dir / \
+        "/home/chowjunwei37/Documents/data/training_set/noisy_train"
+    params_train_path = project_dir / \
+        "/home/chowjunwei37/Documents/data/training_set/params_train"
 
-loader_train = DataLoader(dataset_train, batch_size=batch_size, 
-                shuffle=True, num_workers=3)
-loader_val = DataLoader(dataset_val, batch_size=batch_size, num_workers=3)
+    loader_train = DataLoader(dataset_train, batch_size=batch_size, 
+                    shuffle=True, num_workers=2)
+    loader_val = DataLoader(dataset_val, batch_size=batch_size, num_workers=2)
 
+    # model = torch.hub.load("pytorch/vision:v0.9.0", "resnet34", pretrained=True)
